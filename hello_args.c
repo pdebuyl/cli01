a7ec915ffa5ca2d7a9668bd666f2797a5b012bc7
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-10-13 10:32:08 +0200

    add bash hello and make c look similar

--- hello_args.c
+++ hello_args.c
@@ -2,9 +2,10 @@
 
 int main(int argc, char *argv[]) {
 
-  printf("Number of arguments: %i\n", argc);
+  printf("Number of arguments: %i\n", argc-1);
 
-  for (int i=0 ; i<argc ; i++) printf("%s\n", argv[i]) ;
+  for (int i=1 ; i<argc ; i++) printf("%s ", argv[i]) ;
+  printf("\n");
 
   return 0;
 }
